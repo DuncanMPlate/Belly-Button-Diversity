58368bccd07bac7f244c072c41380ed389b41d91
Duncan Plate <duncanplate@gmail.com>
2021-08-22 18:05:18 -0500

    plotting bar

--- static/js/app.js
+++ static/js/app.js
@@ -10,4 +10,32 @@ let layout = {
 	title: "Bar Chart"
 };
 
-Plotly.newPlot("plot", trace, layout);
+Plotly.newPlot("bar", trace, layout);
+
+//d3.selectAll("#selDataset").on("change", getData);
+
+
+//function getData() {
+  //var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  //var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+//  var data = [];
+
+  //if (dataset == 'us') {
+   //   data = us;
+  //}
+  //else if (dataset == 'uk') {
+    //  data = uk;
+  //}
+  //else if (dataset == 'canada') {
+    //  data = canada;
+  //}
+  // Call function to update the chart
+  //updatePlotly(data);
+//}
+
+// Update the restyled plot's values
+//function updatePlotly(newdata) {
+  //Plotly.restyle("pie", "values", [newdata]);
+//}
