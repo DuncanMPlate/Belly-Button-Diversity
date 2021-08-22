4f97ab7dbb20ebb8a420720b17ab9b34fcc34821
Duncan Plate <duncanplate@gmail.com>
2021-08-22 17:37:37 -0500

    first plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,12 @@
-d3.json("data/samples.json").then(data=>console.log(data))
+data = d3.json("data/samples.json").then(data=>console.log(data));
 
+let trace = {
+	x: data.samples,
+	y: names
+
+}
+let layout = {
+	title: "Bar Chart"
+};
+
+Plotly.newPlot("plot", trace, layout);
